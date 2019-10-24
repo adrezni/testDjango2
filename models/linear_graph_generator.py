bfdaa8bc72e8e90919081bc6f0232c7c74c1549f
eliguidera <guiderae@yahoo.com>
2019-10-24 13:17:51 -0500

    Changed generate_Q_graph() to encode the graph image as a base64 encoded string.

--- models/linear_graph_generator.py
+++ models/linear_graph_generator.py
@@ -1,18 +1,18 @@
 from io import BytesIO
+import base64
 import matplotlib.pyplot as plt
-
 
 
 class LinearGraphGenerator:
     """
     This class generates the quarterly plots.  Strategy is to
-    create a pyplot with multiple figures  for each quarter and to return that pyplot
+    create a pyplot with multiple figures and save each figure as an encoded base64 string.
     """
     @staticmethod
     def generate_Q_graphs(form_dict, data_frame):
         """
         Generate 4 graph images from input parameters form_dict and
-        data frame data_frame.  Return a list of images
+        data frame data_frame.  Return as 4 base64 encoded strings
         """
         for keys, values in form_dict.items():
             print (keys, values)
@@ -23,19 +23,21 @@ class LinearGraphGenerator:
         q2y = [0.2,0.4,0.5,0.6,0.7,0.8,0.9,0.8,0.9,0.5,0.4,0.6]
         q3y = [0.3,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.5,0.6,0.5,0.6]
         q4y = [0.4,0.3,0.5,0.4,0.7,0.8,0.7,0.6,0.5,0.4,0.3,0.1]
-        LinearGraphGenerator.generate_Q_graph(qx, q1y, 1)
-        LinearGraphGenerator.generate_Q_graph(qx, q2y, 2)
-        LinearGraphGenerator.generate_Q_graph(qx, q3y, 3)
-        LinearGraphGenerator.generate_Q_graph(qx, q4y, 4)
+        # End of dummy data
+        encoded1 = LinearGraphGenerator.generate_Q_graph(qx, q1y, 1)
+        encoded2 = LinearGraphGenerator.generate_Q_graph(qx, q2y, 2)
+        encoded3 = LinearGraphGenerator.generate_Q_graph(qx, q3y, 3)
+        encoded4 = LinearGraphGenerator.generate_Q_graph(qx, q4y, 4)
 
-        return plt
+        return encoded1, encoded2, encoded3, encoded4
 
     @staticmethod
     def generate_Q_graph(qx, qy, quarter_no):
         plt.figure(quarter_no)
         plt.scatter(qx, qy)
         plt.title('Quarter' + str(quarter_no))
-        # img = BytesIO()
-        # plt.savefig(img)
-        # img.seek(0)
-        return
+        buffer = BytesIO()
+        plt.savefig(buffer, format="png")
+        buffer.seek(0)
+        encoded = base64.b64encode(buffer.getvalue()).decode("ascii")
+        return encoded
