0c5a533d2ddfc1432556e40fa3579fa79236e1d7
eliguidera <guiderae@yahoo.com>
2019-10-28 14:30:49 -0500

    Created generate_q_graphNew() which does plots of two sets of points per plot.

--- models/linear_graph_generator.py
+++ models/linear_graph_generator.py
@@ -1,6 +1,7 @@
 from io import BytesIO
 import base64
 import matplotlib.pyplot as plt
+import pandas as pd
 
 
 class LinearGraphGenerator:
@@ -22,11 +23,17 @@ class LinearGraphGenerator:
         q2y = [0.2,0.4,0.5,0.6,0.7,0.8,0.9,0.8,0.9,0.5,0.4,0.6]
         q3y = [0.3,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.5,0.6,0.5,0.6]
         q4y = [0.4,0.3,0.5,0.4,0.7,0.8,0.7,0.6,0.5,0.4,0.3,0.1]
+
+        q11y = (pd.Series(q1y) * 0.2).tolist()
+        q21y = (pd.Series(q2y) * 0.2).tolist()
+        q31y = (pd.Series(q3y) * 0.2).tolist()
+        q41y = (pd.Series(q4y) * 0.2).tolist()
+
         # End of dummy data
-        encoded1 = LinearGraphGenerator.generate_Q_graph(qx, q1y, 1)
-        encoded2 = LinearGraphGenerator.generate_Q_graph(qx, q2y, 2)
-        encoded3 = LinearGraphGenerator.generate_Q_graph(qx, q3y, 3)
-        encoded4 = LinearGraphGenerator.generate_Q_graph(qx, q4y, 4)
+        encoded1 = LinearGraphGenerator.generate_Q_graphNew(qx, q1y, qx, q11y, 1)
+        encoded2 = LinearGraphGenerator.generate_Q_graphNew(qx, q2y, qx, q21y,2)
+        encoded3 = LinearGraphGenerator.generate_Q_graphNew(qx, q3y, qx, q31y,3)
+        encoded4 = LinearGraphGenerator.generate_Q_graphNew(qx, q4y, qx, q41y, 4)
 
         return encoded1, encoded2, encoded3, encoded4
 
@@ -34,6 +41,19 @@ class LinearGraphGenerator:
     def generate_Q_graph(qx, qy, quarter_no):
         plt.figure(quarter_no)
         plt.scatter(qx, qy)
+        plt.title('Quarter' + str(quarter_no))
+        buffer = BytesIO()
+        plt.savefig(buffer, format="png")
+        buffer.seek(0)
+        encoded = base64.b64encode(buffer.getvalue()).decode("ascii")
+        return encoded
+
+    @staticmethod
+    def generate_Q_graphNew(qx,qy,qx1,qy1,quarter_no):
+        fig = plt.figure(quarter_no)
+        ax1 = fig.add_subplot(111)
+        ax1.scatter(qx, qy, c='b')
+        ax1.scatter(qx1, qy1, c='g')
         plt.title('Quarter' + str(quarter_no))
         buffer = BytesIO()
         plt.savefig(buffer, format="png")
